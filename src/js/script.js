a7a766ec853de29ee8408953e1d5201c07c38ffe
rfrf198231 <rfrf198231@gmail.com>
2022-09-23 12:22:02 +0200

    dddd

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,108 @@
 // POPULAR
-
-// $(document).ready(function () {
-//     $(".popular_inner").slick({
-//         infinite: true,
-//         slidesToShow: 4,
-//         slidesToScroll: 1,
-//         speed: 500,
-//         // autoplay: true,
-//         autoplaySpeed: 1000,
-//         pauseOnFocus: true,
-//         prevArrow: `<button type="button" class="slick-prev">
-//         <img src="../assets/img/icons/right.svg">
-//         </button>`,
-//         nextArrow: `<button type="button" class="slick-next"><img src="../assets/img/icons/left.svg"></button>`,
-//         // Правила работают от 0 до значения которе мы задали
-//         responsive: [
-//             {
-//                 breakpoint: 1440,
-//                 settings: {
-//                     slidesToShow: 4,
-//                     slidesToScroll: 2,
-//                     infinite: true,
-//                     // dots: true,
-//                 },
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 2,
-//                 },
-//             },
-//             {
-//                 breakpoint: 550,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 1,
-//                     adaptiveHeight: true,
-//                     vertical: true,
-//                 },
-//             },
-
-//             // You can unslick at a given breakpoint now by adding:
-//             // settings: "unslick"
-//             // instead of a settings object
-//         ],
-//     });
-// });
+$(document).ready(function () {
+    $(".popular__inner").slick({
+        slidesToShow: 4,
+        draggable: true,
+        infinite: true,
+        slidesToScroll: 1,
+        speed: 900,
+        vertical: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        pauseOnFocus: true,
+        prevArrow: `<button type="button" class="slick-prev">
+        <img src="../assets/img/icons/right.svg">
+        </button>`,
+        nextArrow: `<button type="button" class="slick-next"><img src="../assets/img/icons/left.svg"></button>`,
+        // Правила работают от 0 до значения которе мы задали
+        responsive: [
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    // dots: true,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    adaptiveHeight: true,
+                    vertical: true,
+                },
+            },
+        ],
+    });
+});
+// Discont
+$(document).ready(function () {
+    $(".discont__inner").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 500,
+        vertical: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        pauseOnFocus: true,
+        prevArrow: `<button type="button" class="slick-prev">
+        <img src="../assets/img/icons/right.svg">
+        </button>`,
+        nextArrow: `<button type="button" class="slick-next"><img src="../assets/img/icons/left.svg"></button>`,
+        // Правила работают от 0 до значения которе мы задали
+        responsive: [
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    // dots: true,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    adaptiveHeight: true,
+                    vertical: true,
+                },
+            },
+        ],
+    });
+});
 
 // PROMO
 // $(document).ready(function () {
@@ -99,50 +155,6 @@
 // });
 // ABOUT
 
-// Discont
-$(document).ready(function () {
-    $(".discont_inner").slick({
-        // infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        speed: 500,
-        // autoplay: true,
-        autoplaySpeed: 1000,
-        pauseOnFocus: true,
-        prevArrow: `<button type="button" class="slick-prev">
-        <img src="../assets/img/icons/right.svg">
-        </button>`,
-        nextArrow: `<button type="button" class="slick-next"><img src="../assets/img/icons/left.svg"></button>`,
-        // Правила работают от 0 до значения которе мы задали
-        responsive: [
-            {
-                breakpoint: 1260,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    // dots: true,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                },
-            },
-            {
-                breakpoint: 550,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    adaptiveHeight: true,
-                    vertical: true,
-                },
-            },
-        ],
-    });
-});
 // Review;
 // $(document).ready(function () {
 //     $(".review__inner").slick({
